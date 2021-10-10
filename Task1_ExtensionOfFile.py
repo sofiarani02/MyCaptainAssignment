d4744955970e3d82a23fb20a4c1453219e91757e
sofiarani02 <92247026+sofiarani02@users.noreply.github.com>
2021-10-10 14:05:54 +0530

    Delete Task1_ExtensionOfFile.py

--- Task1_ExtensionOfFile.py
+++
@@ -1,9 +0,0 @@
-filename = input("Input the Filename: ")
-f = filename.split(".")
-if(f[-1]=="py"):
-    print ("The extension of the file is : Python")
-
-else:    
-    print ("The extension of the file is : " + f[-1])
-
-
